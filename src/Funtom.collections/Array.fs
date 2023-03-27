c6f89ae0b57f0399f71b9add4a946185cd8df2ee
midoliy <midoliy@outlook.jp>
2023-03-27 23:11:19 +0900

    wip

--- src/Funtom.collections/Array.fs
+++ src/Funtom.collections/Array.fs
@@ -18,19 +18,6 @@ module Array =
             for i = 1 to vec128<'T>.Count - 1 do
               if max'' < max'[i] then max'' <- max'[i]
             max''
-
-    //let rec max256<'T when 'T: unmanaged and 'T: struct and 'T: comparison and 'T: (new: unit -> 'T) and 'T:> System.ValueType>
-    //  (c: byref<'T>, to': byref<'T>, max': vec256<'T>) =
-    //    if Unsafe.IsAddressLessThan(&c, &to')
-    //      then
-    //        max256 (&Unsafe.Add(&c, vec256<'T>.Count), &to', vec256.Max(max', vec256.LoadUnsafe(&c)))
-    //      else
-    //        let max' = vec256.Max(max', vec256.LoadUnsafe(&to'))
-    //        let mutable max'' = max'[0]
-    //        for i = 1 to vec256<'T>.Count - 1 do
-    //          if max'' < max'[i] then max'' <- max'[i]
-    //        max''
-
     let rec max256<'T when 'T: unmanaged and 'T: struct and 'T: comparison and 'T: (new: unit -> 'T) and 'T:> System.ValueType>
       (current': byref<'T>, last': byref<'T>, best': vec256<'T>) =
         if Unsafe.IsAddressLessThan(&current', &last')
@@ -54,30 +41,30 @@ module Array =
         let r' = &(ref src)
         let to' = &(Unsafe.Add(&r', src.Length - vec128<^T>.Count))
         Internal.max128(&r', &to', vec128.LoadUnsafe(&r'))
-      //let inline simd_256() = 
-      //  let src = src.AsSpan()
-      //  let mutable current = ref src
-      //  let mutable last = Unsafe.Add(&current, src.Length - vec256<^T>.Count)
-      //  Internal.max256(&current, &last, vec256.LoadUnsafe(&current))
+      let inline simd_256() = 
+       let src = src.AsSpan()
+       let mutable current = ref src
+       let mutable last = Unsafe.Add(&current, src.Length - vec256<^T>.Count)
+       Internal.max256(&current, &last, vec256.LoadUnsafe(&current))
 
       // SIMD256: Very Slow
       // https://github.com/dotnet/runtime/blob/release/8.0-preview3/src/libraries/System.Linq/src/System/Linq/MaxMin.cs#L71
-      let inline simd_256() = 
-        let src = src.AsSpan()
-        let current = &(ref src)
-        let last = &(Unsafe.Add(&current, src.Length - vec256<^T>.Count))
+      // let inline simd_256() = 
+      //   let src = src.AsSpan()
+      //   let current = &(ref src)
+      //   let last = &(Unsafe.Add(&current, src.Length - vec256<^T>.Count))
 
-        let mutable best = vec256.LoadUnsafe(&current)
-        current <- Unsafe.Add(&current, vec256<^T>.Count)
-        while Unsafe.IsAddressLessThan(&last, &current) do
-          best <- vec256.Max(best, vec256.LoadUnsafe(&current))
-          current <- Unsafe.Add(&current, vec256<^T>.Count)
-        best <- vec256.Max(best, vec256.LoadUnsafe(&last))
+      //   let mutable best = vec256.LoadUnsafe(&current)
+      //   current <- Unsafe.Add(&current, vec256<^T>.Count)
+      //   while Unsafe.IsAddressLessThan(&last, &current) do
+      //     best <- vec256.Max(best, vec256.LoadUnsafe(&current))
+      //     current <- Unsafe.Add(&current, vec256<^T>.Count)
+      //   best <- vec256.Max(best, vec256.LoadUnsafe(&last))
 
-        let mutable max = best[0]
-        for i = 1 to vec256<^T>.Count - 1 do
-          if max < best[i] then max <- best[i]
-        max
+      //   let mutable max = best[0]
+      //   for i = 1 to vec256<^T>.Count - 1 do
+      //     if max < best[i] then max <- best[i]
+      //   max
 
 
 
